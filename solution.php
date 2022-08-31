925ae288ba7c84ddb77e6d056f702be6af90216d
Kamil Rafikov <mailbox@kamil-rafik.com>
2022-08-31 12:43:32 +0300

    query values from array by condition

--- solution.php
+++ solution.php
@@ -76,3 +76,77 @@ Sorter::sort($input, 'date');
 print_r($input);
 Sorter::sort($input, 'name');
 print_r($input);
+
+////////////////////////////////////////////
+// task #3
+
+class Query
+{
+    protected static $key;
+
+    protected static $operators = [
+        '===' => '===',
+        '<' => '<',
+        '>' => '>',
+    ];
+
+    public static function get($array, $key, $comparedValue, $operator)
+    {
+        if (
+            !count($array)
+            || !preg_match('/^([a-z]+)$/i', $key)
+            || !preg_match('/^([a-z0-9.]+)$/i', $comparedValue)
+            || !isset(self::$operators[$operator])
+        ) {
+            return [];
+        }
+
+        $method = null;
+        if (is_integer($array[0][$key])) {
+            $method = 'compareIntegers';
+        } elseif (preg_match('/^([0-9]{2})\.([0-9]{2})\.([0-9]{4})$/', $array[0][$key])) {
+            $method = 'compareDates';
+        } else {
+            $method = 'compareStrings';
+        }
+
+        return array_filter($array, function ($value) use (
+            $method,
+            $key,
+            $comparedValue,
+            $operator
+        ) {
+            return self::$method($value[$key], $comparedValue, self::$operators[$operator]);
+        });
+    }
+
+    protected static function compareIntegerValue($a, $b, $operator)
+    {
+        return eval('return ' . $a . ' ' . $operator . ' ' . $b . ';');
+    }
+
+    protected static function compareIntegers($value, $comparedValue, $operator)
+    {
+        return self::compareIntegerValue($value, $comparedValue, $operator);
+    }
+
+    protected static function compareDates($value, $comparedValue, $operator)
+    {
+        return self::compareIntegerValue(strtotime($value), strtotime($comparedValue), $operator);
+    }
+
+    protected static function compareStrings($value, $comparedValue, $operator)
+    {
+        if ($operator === '===') {
+            return eval('return ($value === $comparedValue);');
+        } else {
+            $n = $operator === '>' ? '1' : '-1';
+            return eval('return strcmp($value, $comparedValue) === ' . $n . ';');
+        }
+    }
+}
+
+print_r(Query::get($input, 'id', 2, '>'));
+print_r(Query::get($input, 'name', 'test3', '==='));
+print_r(Query::get($input, 'date', '13.01.2020', '<'));
+print_r(Query::get($input, 'name', 'test3', '>'));
